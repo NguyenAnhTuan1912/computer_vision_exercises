8bce8a5b8af9a8b52c6fe628a6cec72533473bb3
NhanLe0309 <87133056+NhanLe0309@users.noreply.github.com>
2023-10-30 15:56:34 +0700

    Update: add week 04

---
+++ utils/mynum.py
@@ -0,0 +1,17 @@
+class MyNum:
+  """
+  MyNum
+  =====
+
+  Dùng để tạo ra một object để thực hiện tính toán lên số.
+
+  Hỗ trợ:
+  - Tạo random số.
+  - Tạo random mảng.
+  """
+
+  def __init__(self) -> None:
+    pass
+
+  def getRandNum():
+    rand
---
+++ weeks/week_04/prob01.py
@@ -0,0 +1,7 @@
+import sys
+
+# Sử dụng sys để add utils vào mới có thể dùng module bên ngoài.
+# sys.path.append('d:/Hoctap/Computer Vision/source')
+sys.path.append('./')
+
+from utils.myann import MyANN
